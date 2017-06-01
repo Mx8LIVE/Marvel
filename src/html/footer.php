5c2984ba45fc858383b3dd5f8207c03c919e313e
Mx8LIVE <yimsanabria@gmail.com>
2017-06-01 01:25:51 -0500

    Directorio de SPA
    Funcionando
    Falta funcionalidad en buscador, filtros y paginamiento.

---
+++ src/html/footer.php
@@ -0,0 +1,4 @@
+<footer>
+    <a href="index.php"><img src="img/grability.png" alt="Grability"></a>
+    <p>Marvel - <a target="_blank" href="http://grability.com">Grability</a> - 2017 - Todos los derechos reservados<br>Data provided by <a target="_blank" href="https://developer.marvel.com">Marvel</a>. © 2014 Marvel</p>
+</footer>
---
+++ src/html/nav.php
@@ -0,0 +1,10 @@
+<nav class="filtro">
+    <ul>
+        <h3>Filtrar por:</h3><br>
+        <li><input type="radio" name="filtro" id="opt1" checked><label for="opt1"> Todos</label></li>
+        <li><input type="radio" name="filtro" id="opt2"><label for="opt2"> Villano</label></li>
+        <li><input type="radio" name="filtro" id="opt3"><label for="opt3"> Comics</label></li>
+        <li><input type="radio" name="filtro" id="opt4"><label for="opt4"> Super Heroe</label></li>
+    </ul><br>
+    <h3>Mis Listas:</h3>
+</nav>
